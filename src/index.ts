07a86a2ff2b93b65bf3baec617cc0f34c3ad92bb
John Andrew Abala <johnandrew.abala@deped.gov.ph>
2021-11-19 14:30:36 +0800

    add google form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+
+
+
+//////////////////////////////////////////// Source Code of Map 01
+
+WA.chat.sendChatMessage('Good day! Welcome to my Class', 'Mr Abala');
+
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
@@ -6,6 +13,29 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
+
+WA.room.onEnterZone('message', () => {
+    currentPopup =  WA.ui.openPopup("popupRectangle","Welcome to the Technical Drafting Room",[{
+        label: "Yes",
+        className: "normal",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+})
+
+WA.room.onLeaveZone('message', closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
